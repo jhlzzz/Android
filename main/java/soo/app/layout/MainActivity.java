9b81e214fdad22a1371cbda8822c3fb44a6c5f6e
jhlzzz <98371537+jhlzzz@users.noreply.github.com>
2022-03-02 12:57:59 +0900

    Add files via upload

---
+++ main/java/soo/app/layout/MainActivity.java
@@ -0,0 +1,17 @@
+package soo.app.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.linear);
+        setContentView(R.layout.relative);
+
+    }
+}
